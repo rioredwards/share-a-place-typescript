21daa6ec310ea6b6aa3d622420b204fcef500466
Rio Edwards <rioredwards@gmail.com>
2023-03-16 12:47:24 -0700

    Add global MapState class. Rendering map on screen and searching coordinates working

--- src/app.ts
+++ src/app.ts
@@ -2,14 +2,82 @@ import axios from "axios";
 import { GOOGLE_API_KEY } from "./key";
 // import { Loader } from "@googlemaps/js-api-loader";
 
+/* DOM Elements */
 const form = document.querySelector("form")! as HTMLFormElement;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
-let mapEl = document.getElementById("map")! as HTMLDivElement;
+const myMapEl = document.getElementById("map")! as HTMLDivElement;
 
+/* Types */
 type GoogleGeocodingResponse = {
   results: { geometry: { location: { lat: number; lng: number } } }[];
   status: "OK" | "ZERO_RESULTS";
 };
+type Coords = { lat: number; lng: number };
+
+/* Global State (Bad Practice) */
+let myMap: MapState;
+let portlandCoords: Coords = { lat: 45.515232, lng: -122.6783853 };
+// let userCoords: Coords | null = null;
+let initZoom: number = 4;
+
+// Singleton for managing the map state
+class MapState {
+  private static instance: MapState;
+  private map: google.maps.Map;
+  private mapEl: HTMLDivElement;
+  private coords: Coords;
+  private zoom: number;
+
+  // Setter for the coords property
+  set coordinates(newCoords: Coords) {
+    this.coords = newCoords;
+  }
+
+  // Getter for the coords property
+  get coordinates() {
+    return this.coords;
+  }
+
+  // setter for the zoom property
+  set zoomLevel(newZoom: number) {
+    this.zoom = newZoom;
+  }
+
+  private constructor(
+    newMapEl: HTMLDivElement,
+    newCoords: Coords,
+    newZoom: number
+  ) {
+    this.mapEl = newMapEl;
+    this.coords = newCoords;
+    this.zoom = newZoom;
+    this.map = new google.maps.Map(this.mapEl, {
+      zoom: this.zoom,
+      center: this.coords,
+    });
+  }
+
+  static getInstance(
+    newMapEl: HTMLDivElement,
+    newCoords: Coords,
+    newZoom: number
+  ) {
+    if (!this.instance) {
+      this.instance = new MapState(newMapEl, newCoords, newZoom);
+    }
+    console.log("MapState instance:", this.instance);
+    console.log("MapState instance map:", this.instance.map);
+    return this.instance;
+  }
+
+  render() {
+    this.map = new google.maps.Map(this.mapEl, {
+      zoom: this.zoom,
+      center: this.coords,
+    });
+    console.log("rendering map:", this.map);
+  }
+}
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
@@ -28,48 +96,22 @@ function searchAddressHandler(event: Event) {
       }
       const coordinates = response.data.results[0].geometry.location;
       console.log("coordinates: ", coordinates);
+      myMap.coordinates = coordinates;
+      myMap.zoomLevel = 16;
+      myMap.render();
     })
     .catch((err) => {
       alert(err.message);
       console.log(err);
     });
-
-  //   // Load map from Google Maps API
-  //   const loader = new Loader({
-  //     apiKey: GOOGLE_API_KEY,
-  //     version: "weekly",
-  //   });
-
-  //   loader.load().then(() => {
-  //     const map = new google.maps.Map(
-  //       document.getElementById("map") as HTMLElement,
-  //       {
-  //         center: { lat: -34.397, lng: 150.644 },
-  //         zoom: 8,
-  //       }
-  //     );
-  //   });
 }
 
 form.addEventListener("submit", searchAddressHandler);
 
 // Initialize and add the map
 function initMap(): void {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
-  const map = new google.maps.Map(mapEl, {
-    zoom: 4,
-    center: uluru,
-  });
-
-  // The marker, positioned at Uluru
-  const marker = new google.maps.Marker({
-    position: uluru,
-    map: map,
-  });
+  myMap = MapState.getInstance(myMapEl, portlandCoords, initZoom);
 }
-
 declare global {
   interface Window {
     initMap: () => void;
